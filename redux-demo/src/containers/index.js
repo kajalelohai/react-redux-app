ffd67ec5b7a412465f320e7aac0b790457c62df8
Elohai Kajal <kl.negi03@gmail.com>
2018-09-23 17:48:41 +0530

    feat: add input element

--- redux-demo/src/containers/index.js
+++ redux-demo/src/containers/index.js
@@ -4,10 +4,18 @@ import { connect } from 'react-redux';
 
 class Main extends React.Component {
   render() {
+    const handleChange = (e) => {};
+
     return (
       <div>
         <h1 className="heading"> INCREMENT/DECREMENT</h1>
         <h2 className="display-value"> {this.props.value} </h2>
+        <input
+          type="text"
+          className="input-screen"
+          value={value}
+          onChange={handleChange}
+        />
       </div>
     );
   }
